46d2fb86b4f4200a2006e5e9b794daf31e192e6d
Ahmed Naser <ahmed@ahmednaser.me>
2022-02-12 19:46:35 +0200

    Init e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,60 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { AuthorsService } from '../src/authors/authors.service';
+import { ArticlesService } from '../src/articles/articles.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Author } from '../src/authors/entities/author.entity';
+import { authorModel } from '../src/authors/test/mocks/author.model';
+import { Article } from '../src/articles/entities/article.entity';
+import { articleModel } from '../src/articles/test/mocks/article.model';
 
-describe('AppController (e2e)', () => {
+describe('Author Controller (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+      providers: [
+        AuthorsService,
+        {
+          provide: getModelToken(Author.name),
+          useValue: authorModel
+        },
+        {
+          provide: getModelToken(Article.name),
+          useValue: articleModel
+        }
+      ]
+    })
+      // .overrideProvider(AuthorsService)
+      // .useValue({
+      //   provide: getModelToken(Author.name),
+      //   useValue: authorModel
+      // })
+      // .overrideProvider(ArticlesService)
+      // .useValue({
+      //   provide: getModelToken(Article.name),
+      //   useValue: articleModel
+      // })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
+  it('/ (GET)', (done) => {
+    request(app.getHttpServer())
+      .get('/v1/authors')
       .expect(200)
-      .expect('Hello World!');
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toEqual([]);
+        done();
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
